6304a5c22af6e115c7d118142bd84f3f066901a8
liziting <liziting@followme.cn>
2019-04-10 19:46:03 +0800

    add addr

--- handler/handler.go
+++ handler/handler.go
@@ -11,9 +11,17 @@ import (
 var token = "https://oapi.dingtalk.com/robot/send?access_token=48f97d91f4fa1a31b7720e0ae30d76217151fc1676c9e5efd8ab034c0b86a6a1"
 
 func ListenGrafana(in *grafanastruct.FromGrafana) {
-	url := strings.Replace(in.RuleURL, "localhost", "10.0.0.30", 1)
+	index := strings.Index(in.RuleURL[8:], string(47))
+	fmt.Println(index,string(47),)
+	url := "http://120.76.198.104:3080" + in.RuleURL[index+8:]
+	fmt.Println(in.RuleURL)
+	fmt.Println(url)
 	msg := fmt.Sprintf(dingdingSay, in.Message, in.Title, in.ImageURL, url)
-	http.Post(token, "application/json", bytes.NewBuffer([]byte(msg)))
+	rsp,err:=http.Post(token, "application/json", bytes.NewBuffer([]byte(msg)))
+	if err!=nil{
+		fmt.Println(err)
+	}
+	fmt.Println(rsp)
 }
 
 func handlerUrl(url string) {
